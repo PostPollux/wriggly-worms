04cef42fe060c0fc36b0bd87d4fc71423484cbf4
Johannes Wuensch <j.wuensch@protonmail.com>
2020-03-05 23:00:08 +0100

    improve food distribution

--- Map/Map.gd
+++ Map/Map.gd
@@ -14,13 +14,8 @@ onready var MapSwitcher = $MapSwitcher
 
 ### VARIABLES
 
-
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-var food_spawn_radius : float = 5000
+var food_start_amount = 2000
+var map_size : int = 5000
 
 
 
@@ -28,27 +23,43 @@ var food_spawn_radius : float = 5000
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	for i in range(0,1000):
+	var iterator : int = 0
+	
+	for i in range(0, food_start_amount):
 		
-		add_food_at_random_position()
-
+		if iterator == 0:
+			add_food_at_random_position_radial_simple()
+		else:
+			add_food_at_random_position_square()
+		
+		iterator = (iterator + 1) % 2
+		
 
 func next_background() -> void:
 	MapSwitcher.activate_next()
 
 
 
-
-func add_food_at_random_position():
+# Distribution is not even. There will be more food in the middle then on the periphery 
+func add_food_at_random_position_radial_simple():
 	
 	var Food = FoodRes.instance()
 	
 	var random_direction_vector : Vector2 = Vector2(randf() * 2 - 1, randf() * 2 - 1).normalized()
 	
-	Food.position = self.position + random_direction_vector * (randf() * food_spawn_radius)
+	Food.position = self.position + random_direction_vector * (randf() * (map_size / 2))
 	
 	self.add_child(Food)
+
+
+# Even distribution in a square
+func add_food_at_random_position_square():
 	
+	var Food = FoodRes.instance()
+	
+	Food.position = (self.position - Vector2(map_size / 2, map_size / 2)) + Vector2(randi() % map_size, randi() % map_size )
+	
+	self.add_child(Food)
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
